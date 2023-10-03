60b9186ca7dade6192c7ae1ab258b7ddcb3c2b78
yuri-silveiraa <yuri.pires.silveira@gmail.com>
2023-10-03 19:54:17 -0300

    node-test

---
+++ src/libs/Math.ts
@@ -0,0 +1,15 @@
+export const Math = {
+    sum: (n1: number, n2: number) => {
+        return n1 + n2;
+    },
+    sub: (n1: number, n2: number) => {
+        return n1 - n2;
+    },
+    div: (n1: number, n2: number) => {
+        if(n2 == 0) return false;
+        return n1 / n2;
+    },
+    mut: (n1: number, n2: number) => {
+        return n1 * n2;
+    }
+}
